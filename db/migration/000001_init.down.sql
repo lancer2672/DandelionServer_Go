8a50271f691e0835be4a79a527a366ca149c24a2
lancer2672 <jbkhanhtran@gmail.com>
2024-03-19 22:50:30 +0700

    generate grpc code

--- db/migration/000001_init.down.sql
+++ db/migration/000001_init.down.sql
@@ -7,6 +7,7 @@ ALTER TABLE "movie_genres" DROP CONSTRAINT IF EXISTS "movie_genres_genre_id_fkey
 ALTER TABLE "movie_genres" DROP CONSTRAINT IF EXISTS "movie_genres_movie_id_fkey";
 ALTER TABLE "movie_history" DROP CONSTRAINT IF EXISTS "movie_history_users_id_fkey";
 ALTER TABLE "movie_history" DROP CONSTRAINT IF EXISTS "movie_history_movies_id_fkey";
+ALTER TABLE "movies" DROP CONSTRAINT IF EXISTS "movies_series_id_fkey";
 
 -- Drop tables
 DROP TABLE IF EXISTS "movie_history";
