935462739cc7a4f8a933c1d3c41387e5b5b93faa
lancer2672 <jbkhanhtran@gmail.com>
2024-03-25 16:39:35 +0700

    remove Gin

--- api/request/genre_request.go
+++
@@ -1,6 +0,0 @@
-package requests
-
-type GetListGenresRequest struct {
-	Limit  int64 `form:"limit,default=10"`
-	Offset int64 `form:"offset"`
-}
--- api/request/upload_request.go
+++
@@ -1,7 +0,0 @@
-package requests
-
-import "mime/multipart"
-
-type UploadFileRequest struct {
-	File *multipart.FileHeader `form:"file" binding:"required"`
-}
