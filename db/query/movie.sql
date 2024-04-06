2ddf67b6d776b4e69011471d9e5c5579e028309a
lancer2672 <jbkhanhtran@gmail.com>
2024-04-06 15:41:56 +0700

    update queries

--- db/query/movie.sql
+++ db/query/movie.sql
@@ -1,5 +1,23 @@
+
+-- name: GetMovie :one
+WITH movie_genres AS (
+  SELECT movie_id, ARRAY_AGG(genres.name) AS genres
+  FROM movie_genres
+  JOIN genres ON movie_genres.genre_id = genres.id
+  GROUP BY movie_id
+)
+SELECT movies.*, movie_genres.genres FROM movies
+LEFT JOIN movie_genres ON movies.id = movie_genres.movie_id
+WHERE movies.id = $1 LIMIT 1;
+
 -- name: GetMoviesByGenre :many
-SELECT * FROM movies
+WITH movie_genres AS (
+  SELECT movie_id, ARRAY_AGG(genres.name) AS genres
+  FROM movie_genres
+  JOIN genres ON movie_genres.genre_id = genres.id
+  GROUP BY movie_id
+)
+SELECT movies.*, movie_genres.genres FROM movies
 JOIN movie_genres ON movies.id = movie_genres.movie_id
 WHERE movie_genres.genre_id = $1
 ORDER BY movies.id
@@ -7,59 +25,71 @@ LIMIT $2
 OFFSET $3;
 
 -- name: GetMoviesBySerie :many
-SELECT * FROM movies
-JOIN  movies_series ON movies.id = movies_series.movie_id
-WHERE movies_series.id = $1
+WITH movie_genres AS (
+  SELECT movie_id, ARRAY_AGG(genres.name) AS genres
+  FROM movie_genres
+  JOIN genres ON movie_genres.genre_id = genres.id
+  GROUP BY movie_id
+)
+SELECT movies.*, movie_genres.genres FROM movies
+JOIN movies_series ON movies.id = movies_series.movie_id
+LEFT JOIN movie_genres ON movies.id = movie_genres.movie_id
+WHERE movies_series.series_id = $1
 ORDER BY movies.id
 LIMIT $2
 OFFSET $3;
 
--- name: GetMovie :one
-SELECT * FROM movies
-WHERE id = $1 LIMIT 1;
-
 -- name: GetListMovies :many
-SELECT * FROM movies
-ORDER BY id
+WITH movie_genres AS (
+  SELECT movie_id, ARRAY_AGG(genres.name) AS genres
+  FROM movie_genres
+  JOIN genres ON movie_genres.genre_id = genres.id
+  GROUP BY movie_id
+)
+SELECT movies.*, movie_genres.genres FROM movies
+LEFT JOIN movie_genres ON movies.id = movie_genres.movie_id
+ORDER BY movies.id
 LIMIT $1
 OFFSET $2;
 
-
 -- name: GetRecentMovies :many
-SELECT * FROM movies
+WITH movie_genres AS (
+  SELECT movie_id, ARRAY_AGG(genres.name) AS genres
+  FROM movie_genres
+  JOIN genres ON movie_genres.genre_id = genres.id
+  GROUP BY movie_id
+)
+SELECT movies.*, movie_genres.genres FROM movies
+LEFT JOIN movie_genres ON movies.id = movie_genres.movie_id
 ORDER BY movies.created_at DESC
 LIMIT $1
 OFFSET $2;
 
 -- name: SearchMovies :many
-SELECT * FROM movies
+WITH movie_genres AS (
+  SELECT movie_id, ARRAY_AGG(genres.name) AS genres
+  FROM movie_genres
+  JOIN genres ON movie_genres.genre_id = genres.id
+  GROUP BY movie_id
+)
+SELECT movies.*, movie_genres.genres FROM movies
+LEFT JOIN movie_genres ON movies.id = movie_genres.movie_id
 WHERE movies.title LIKE '%' || $1 || '%'
 ORDER BY movies.id
 LIMIT $2
 OFFSET $3;
 
--- name: CreateMovie :exec
-INSERT INTO movies
- (title, duration, description, actor_avatars, trailer, file_path, thumbnail, views, stars)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
-
--- name: UpdateMovie :one
-UPDATE movies 
-SET 
-    title = COALESCE(sqlc.narg('title'), title),
-    description = COALESCE(sqlc.narg('description'), description),
-    file_path = COALESCE(sqlc.narg(file_path), file_path),
-    thumbnail = COALESCE(sqlc.narg(thumbnail), thumbnail)
-WHERE 
-    id = sqlc.arg(id)
-RETURNING *;
-
 -- name: GetWatchingMovies :many
-SELECT movies.* , movie_history.watched_duration, movie_history.last_watched FROM movies
+WITH movie_genres AS (
+  SELECT movie_id, ARRAY_AGG(genres.name) AS genres
+  FROM movie_genres
+  JOIN genres ON movie_genres.genre_id = genres.id
+  GROUP BY movie_id
+)
+SELECT movies.*, movie_genres.genres, movie_history.watched_duration, movie_history.last_watched FROM movies
 JOIN movie_history ON movies.id = movie_history.movie_id
+LEFT JOIN movie_genres ON movies.id = movie_genres.movie_id
 WHERE movie_history.user_id = $1 AND (movie_history.watched_duration / movies.duration) > 0.1
 ORDER BY movie_history.last_watched DESC
 LIMIT $2
 OFFSET $3;
-
-
